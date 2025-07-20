1ce1bdd1814432e61abb20032e61887d1b0984f3
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-07-21 00:38:11 +0300

    Initialize sequelize connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import pool from './db.js';
+const express = require('express');
+const dotenv = require('dotenv');
+
+const { sequelize } = require('./sequelize/models');
 
 dotenv.config();
 
@@ -10,14 +11,23 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`[INFO] Server is running on port ${PORT}`);
-});
+const connectDB = async() => {
+  console.log("[INFO] Checking database connection...");
+  try {
+    await sequelize.authenticate();
+    console.log("[INFO] Database connection established.");
+  } catch(e) {
+    console.log("[ERROR] Database connection failed: ", e);
+    process.exit(1);
+  }
+}
 
-pool.query("SELECT NOW()")
-    .then((res) => {
-        console.log("[INFO] Database connected at: ", res.rows[0].now);
-    })
-    .catch((err) => {
-        console.log("[ERROR] Database connection error: ", err);
-    });
+(async () => {
+
+  await connectDB();
+  console.log(`[INFO] Attempting to run the server on port ${PORT}...`);
+  app.listen(PORT, () => {
+      console.log(`[INFO] Server is running on port ${PORT}`);
+  });
+
+})();
