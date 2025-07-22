ee5eb40fd3c52c911dbe1282fa76d5759a61e588
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-07-22 19:09:10 +0300

    Implement user sign in from backend using JWT

--- backend/src/routes/users/index.js
+++ backend/src/routes/users/index.js
@@ -1,6 +1,12 @@
+const { Op } = require("sequelize");
 const { sequelize } = require("../../sequelize/models");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const env = require("../../env");
+
+const User = sequelize.models.User;
 
 router.post("/sign-up", async (req, res) => {
   const { display_name, email, password, username } = req.body;
@@ -10,7 +16,7 @@ router.post("/sign-up", async (req, res) => {
   }
 
   try {
-    await sequelize.models.User.create({
+    await User.create({
       display_name,
       email,
       password,
@@ -30,7 +36,54 @@ router.post("/sign-up", async (req, res) => {
         error: err.errors?.[0]?.message || "Validation error.",
       });
     }
-    console.error("[ERROR] Sign-up error:", err.message);
+
+    console.error("[ERROR] Sign up error:", err.message);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { identifier, password } = req.body; // identifier = email or username
+
+  if (!identifier || !password) {
+    return res
+      .status(400)
+      .json({ error: "Email/Username and password are required." });
+  }
+
+  try {
+    // find user where email = identifier or username = identifier
+    const loggingUser = await User.findOne({
+      where: {
+        [Op.or]: [{ email: identifier }, { username: identifier }],
+      },
+    });
+
+    if (!loggingUser) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+
+    const isMatch = await bcrypt.compare(password, loggingUser.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+    const token = jwt.sign(
+      {
+        id: loggingUser.id,
+        email: loggingUser.email,
+        username: loggingUser.username,
+      },
+      env.JWT_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res
+      .status(201)
+      .json({ msg: "User authentication is successful.", token: token });
+  } catch (err) {
+    console.error("[ERROR] Login error:", err.message);
     res.status(500).json({ error: "Internal server error." });
   }
 });
