d27559e1a8ffd2dd6f0f0e9c990e3834dd8d1018
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-07-22 16:31:08 +0300

    Add constraints & validations to User model

--- backend/src/sequelize/models/user.js
+++ backend/src/sequelize/models/user.js
@@ -1,5 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -19,15 +21,67 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      display_name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      username: DataTypes.STRING,
+      display_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Display name is required." },
+          notEmpty: { msg: "Display name cannot be empty." },
+          is: {
+            args: /^[a-zA-Z\s]*$/,
+            msg: "Display name must contain only letters and whitespaces.",
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: { name: "unique_email", msg: "Email already used." },
+        validate: {
+          notNull: { msg: "Email is required." },
+          notEmpty: { msg: "Email cannot be empty." },
+          isEmail: { msg: "Invalid email address" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: { name: "unique_username", msg: "Username already used." },
+        validate: {
+          notNull: { msg: "Username is required." },
+          notEmpty: { msg: "Username cannot be empty." },
+          is: {
+            args: /^[\w.]+$/,
+            msg: "Username must only contain alphanumerical, dot or underscore.",
+          },
+        },
+      },
     },
     {
       sequelize,
       modelName: "User",
       underscored: true,
+      hooks: {
+        beforeCreate: async (user) => {
+          // store email lowercase
+          if (user.email) {
+            user.email = user.email.toLowerCase();
+          }
+          // store username lowercase
+          if (user.username) {
+            user.username = user.username.toLowerCase();
+          }
+          if (user.password) {
+            // hash password
+            const salt = await bcrypt.genSalt();
+            user.password = await bcrypt.hash(user.password, salt);
+          }
+        },
+      },
     }
   );
   return User;
