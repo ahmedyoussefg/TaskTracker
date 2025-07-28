8017739000d0dc1c1b0fd52b6643d2a2ea07d15f
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-07-28 20:23:14 +0300

    Write Middleware to authenticate user token

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const env = require("../env");
+const { sequelize } = require("../sequelize/models");
+
+const User = sequelize.models.User;
+
+async function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "Access token missing." });
+  }
+  try {
+    const payload = jwt.verify(token, env.JWT_KEY);
+    const currentUser = await User.findByPk(payload.id);
+    if (!currentUser) {
+      return res.status(401).json({ error: "Invalid user token." });
+    }
+    req.currentUser = currentUser;
+    next();
+  } catch (err) {
+    console.error("[ERROR] JWT verification: ", err.message);
+    return res.status(403).json({ error: "Invalid or expired token." });
+  }
+}
+
+module.exports = authenticateToken;
