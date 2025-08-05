ba808cbed92022c404a6284ca3ec3940275aa549
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-08-05 16:18:47 +0300

    Use Winston and Morgan loggers

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,34 +3,43 @@ const env = require("./env");
 const users = require("./routes/users");
 const tasks = require("./routes/tasks");
 const cors = require("cors");
+const { logger, errorLogger, requestLogger } = require("./logger");
 
 const { sequelize } = require("./sequelize/models");
 
 const app = express();
-
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(requestLogger);
+}
 
 app.use("/api/users", users);
 app.use("/api/tasks", tasks);
 
+app.use(errorLogger);
+
 const PORT = env.PORT || 3000;
 
 const connectDB = async () => {
-  console.log("[INFO] Checking database connection...");
+  logger.info("Checking database connection...");
   try {
     await sequelize.authenticate();
-    console.log("[INFO] Database connection established.");
+    logger.info("Database connection established.");
   } catch (e) {
-    console.log("[ERROR] Database connection failed: ", e);
+    logger.error("Database connection failed:", {
+      message: e.message,
+      stack: e.stack,
+    });
     process.exit(1);
   }
 };
 
 (async () => {
   await connectDB();
-  console.log(`[INFO] Attempting to run the server on port ${PORT}...`);
+  logger.info(`Attempting to run the server on port ${PORT}...`);
   app.listen(PORT, () => {
-    console.log(`[INFO] Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
   });
 })();
